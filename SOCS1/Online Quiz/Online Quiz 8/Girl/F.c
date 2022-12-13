805197102007934e79d01896a55aa573084995c0
GilbertKrantz <wilbertchandra.official@gmail.com>
2022-12-13 23:50:13 +0700

    Fix Girl SOCS dan Tugas ITDS Session 9

--- SOCS1/Online Quiz/Online Quiz 8/Girl/F.c
+++ SOCS1/Online Quiz/Online Quiz 8/Girl/F.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
     for (int i = 1; i <= T; i++)
     {
         int count = 0, j, k;
-        char str[10000];
+        char str[1000000];
         scanf("%s", str);
 
         for (j = 0; j < strlen(str); j++)
